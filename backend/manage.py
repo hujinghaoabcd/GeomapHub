42f35637b9a362ccc03f364bb8b757dbead59038
Jinghao Hu <731409213@qq.com>
2026-07-28 13:23:57 +0800

    Add Django management entrypoint

---
+++ backend/manage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from __future__ import annotations
+
+import os
+import sys
+
+
+def main() -> None:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
